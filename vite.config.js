e4cc0de12fd50df36a17ceba75b2812776609880
pwaycuilis <pwaycuilis@gmail.com>
2023-07-12 17:16:04 -0500

    updated dist foldier and package json trying to get dist build working for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build: {
+  //   outDir: 'dist',
+  //   rollupOptions: {
+  //     input: {
+  //       main: './src/main.jsx',
+  //     },
+  //   }
+  // }
+  // build: {
+  //   outDir: 'dist',
+  //   emptyOutDir: true,
+  //   rollupOptions: {
+  //     input: {
+  //       main: './src/main.jsx',
+
+  //     },
+  //   },
+  // },
+  
 })
